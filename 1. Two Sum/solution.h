d117101aaab42b9d0db1afb564c86d7dea146308
MuuYiz <xairzy@gmail.com>
2020-03-25 03:36:23 +0800

    add 1

--- 1. Two Sum/solution.h
+++ 1. Two Sum/solution.h
@@ -1,6 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for(int a = 0; a < nums.size(); ++a)
+        {
+            m[nums[a]] = a;
+        }
+        
+        for(int a = 0; a < nums.size(); ++a)
+        {
+            if(m.count(target - nums[a]) && (m[target - nums[a]] != a))
+            {
+                return {a, m[target - nums[a]]};
+            }
+        }
+        return {};
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map m;
         for (int i = 0; i < nums.size(); ++i)
         {
@@ -13,3 +34,4 @@ public:
         return {};
     }
 };
+*/
