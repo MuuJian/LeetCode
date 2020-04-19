a90d2047998da1a5b25c0dc7595540732e0a55ae
MuuYiz <xairzy@gmail.com>
2020-04-19 08:58:33 +0800

    1008 done

---
+++ 1008. Construct Binary Search Tree from Preorder Traversal/solution.h
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        return Helper(preorder, 0, preorder.size());
+    }
+    
+    TreeNode* Helper(vector<int>& preorder, int start, int end)
+    {
+        if(start >= end)
+            return NULL;
+        TreeNode* node = new TreeNode(preorder[start]);
+        int spiltnum = start;
+        while(spiltnum < end && preorder[spiltnum] <= preorder[start])
+            ++spiltnum;
+        cout << spiltnum << " ";
+        node -> left = Helper(preorder, start + 1, spiltnum);
+        node -> right = Helper(preorder, spiltnum, end);
+        return node;
+    }
+};
---
+++ 147. Insertion Sort List/solution.h
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode *dummy = new ListNode(0), *curr = dummy;
+        while(head)
+        {
+            ListNode *t = head -> next;
+            curr = dummy;
+            while(curr -> next != NULL && curr -> next -> val <= head -> val)
+            {
+                curr = curr -> next;
+            }
+            head -> next = curr -> next;
+            curr -> next = head;
+            head = t;
+        }
+        return dummy -> next;
+    }
+};
