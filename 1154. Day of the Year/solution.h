fd8290cb701f09b9f87139be6970ac5019f3a495
MuuYiz <xairzy@gmail.com>
2020-04-20 08:30:16 +0800

    1154

---
+++ 1154. Day of the Year/solution.h
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dayOfYear(string date) {
+        int year = stoi(date.substr(0, 4));
+        int month = stoi(date.substr(5, 2));
+        int day = stoi(date.substr(8));
+        return countDays(day, month, year);
+    }
+    bool isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+    int countDays(int day, int month, int year) {
+        int res = 0;
+        int n = isLeapYear(year);
+        while (month > 1) {
+            res += daysOfMonth[n][month - 2];
+            month--;
+        }
+        res += day;
+        return res;
+    }
+    vector<vector<int>> daysOfMonth{
+        {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+        {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+    };
+};
